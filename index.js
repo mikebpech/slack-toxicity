f63af9cc3184120ee7b9ded1299087752db77c68
Mike Pechousek <mikel@playeverland.com>
2020-03-11 13:07:14 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const bodyParser = require('body-parser');
+const app = require('express')();
+
+let server;
+server = require('http').createServer(app);
+
+app.use((req, res, next) => {
+  // Set CORS headers so that the React SPA is able to communicate with this server
+  
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,POST,PUT,PATCH,DELETE,OPTIONS',
+    );
+    
+    res.setHeader('Access-Control-Allow-Headers', ['Content-Type']);
+    next();
+  });
+
+app.get('/toxicity', (req, res) => {
+  console.log(req);
+})
